d1dd478183850ee5e422ca16d1901076bacb955a
Gisele <giskey11@gmail.com>
2020-03-08 13:47:48 -0400

    Add server.js and handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,42 @@
 let express = require("express");
+
+let router = express.Router();
+
+let burger = require("../models/burger");
+
+//create all our routes
+//get all
+router.get("/", (req, res) => {
+  burger.all(function(data) {
+    let hbsObject = {
+        burgers: data
+      };
+      console.log("Its working");
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+  });
+});
+
+//add burger
+router.post("/api/burgers", (req, res) => {
+  burger.create(["burger_name"], [req.body.name], function(result) {
+    if (err) {
+      console.log("create error :" + err);
+    }
+    res.json(result);
+  });
+});
+//update buger
+router.put("/api/burgers/:id", (req, res) => {
+    let condition = "id = "+ req.params.id;
+    console.log("condition to update :" + condition);
+
+    burger.update({devoured: true}, function(result) {
+      if (err) {
+        console.log("update error :" + err);
+      }
+      res.json(result);
+    });
+  });
+
+  module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,22 @@
+// import the ORM to create functions to interact with the DB
+let orm = require("../config/orm");
+
+let burger = {
+    all: function(cb){
+        orm.all("burgers", function(res){
+            cb(res);
+        });
+    },
+    create: function(cols, vals, cb){
+        orm.create("burgers", cols, vals, function(res){
+            cb(res);
+        });
+    },
+    update: function(objcolVals, condition, cb){
+        orm.update("burgers", condition, function(res){
+            cb(res);
+        });
+    }
+}
+
+module.exports = burger;
